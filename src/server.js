232ac5176ff1d01304a4ca7996fc6b0a477c8f2c
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2024-02-26 18:55:06 +0600

    testing middleware

--- src/server.js
+++ src/server.js
@@ -5,10 +5,28 @@ const morgen = require("morgan");
 
 //middleware
 app.use(morgen("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const isLoggin = (req, res, next) => {
+    const login = 0;
+    if (login) next();
+    else {
+        return res.status(401).send({
+            message: "please login first",
+        });
+    }
+};
 
 app.get("/users", (req, res) => [
     res.send({
         message: "Hi users",
+    }),
+]);
+
+app.get("/profile", (req, res) => [
+    res.status(200).send({
+        message: "Hi Profile",
     }),
 ]);
 app.listen(PORT, () => {
