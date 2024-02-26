4bc915f54d884783cb87e9759871c6c8ac126912
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2024-02-26 19:13:10 +0600

    client and server error handeler

--- src/server.js
+++ src/server.js
@@ -2,17 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const morgen = require("morgan");
+const bodyParser = require("body-parser");
 
 //middleware
 app.use(morgen("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const isLoggin = (req, res, next) => {
-    const login = 0;
+    const login = 1;
     if (login) next();
     else {
-        return res.status(401).send({
+        return res.status(401).json({
             message: "please login first",
         });
     }
@@ -25,10 +26,22 @@ app.get("/users", (req, res) => [
 ]);
 
 app.get("/profile", (req, res) => [
-    res.status(200).send({
+    res.status(200).json({
         message: "Hi Profile",
     }),
 ]);
+
+//client error handeler
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Not Found." });
+    next();
+});
+
+// server error handeler
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send("Internal Server Error.");
+});
 app.listen(PORT, () => {
     console.log(`SERVER is running on port ${PORT}`);
 });
