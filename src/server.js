76f5e2637389fca6045ba01fb16c5f33738d479b
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2024-02-26 18:30:39 +0600

    install morgan and nodemon

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const morgen = require("morgan");
 
+//middleware
+app.use(morgen("dev"));
+
+app.get("/users", (req, res) => [
+    res.send({
+        message: "Hi users",
+    }),
+]);
 app.listen(PORT, () => {
     console.log(`SERVER is running on port ${PORT}`);
 });
