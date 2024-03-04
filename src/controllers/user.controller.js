8c0ceeaa7b22207a1e567824e17ca8cb6968fe98
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2024-03-04 16:53:22 +0600

    added paigination and searching and filtering apply

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,47 @@
-const users = require("../models/user.model");
-const getUsers = (req, res, next) => {
+const User = require("../models/user.model");
+const createError = require("http-errors");
+const getUsers = async (req, res, next) => {
     try {
-        res.status(200).json({
+        const search = req.query.search || "";
+        const page = Number(req.query.page) || 1;
+        const limit = req.query.limit || 5;
+
+        const searchRegEx = new RegExp(".*" + search + ".*", "i");
+
+        // admin liftering and searching unsing name, email, phone
+        const filter = {
+            isAdmin: { $ne: true },
+            $or: [
+                { name: { $regex: searchRegEx } },
+                { email: { $regex: searchRegEx } },
+                { phone: { $regex: searchRegEx } },
+            ],
+        };
+
+        // apply  peojection
+        const options = {
+            password: 0,
+        };
+
+        const users = await User.find(filter, options)
+            .limit(limit)
+            .skip((page - 1) * limit);
+
+        if (!users) throw createError(404, "User not found.");
+
+        // calculate toatl user length
+        const userCount = await User.find(filter).countDocuments();
+
+        return res.status(200).json({
             message: "user fetched successfully",
             users,
+            paigination: {
+                totalPage: Math.ceil(userCount / limit),
+                currentPage: page,
+                previousPage: page - 1 > 0 ? page - 1 : null,
+                nextPage:
+                    page + 1 <= Math.ceil(userCount / limit) ? page + 1 : null,
+            },
         });
     } catch (error) {
         next(error);
